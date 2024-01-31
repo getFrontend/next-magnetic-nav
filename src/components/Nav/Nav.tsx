c2bfc9d6bae2bad7bf9dbfe252fdf38f4167a484
Sergey UP <sergey10goals@gmail.com>
2024-01-31 21:44:52 +0200

    added motion from framer-motion

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,8 @@
 "use client"
+import { cn } from "@/lib/utils";
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
 
 export default function Nav() {
   const links = [
@@ -16,17 +19,24 @@ export default function Nav() {
       name: "contact"
     },
   ]
+  const pathname = usePathname();
+  const MotionLink = motion(Link);
 
   return (
     <nav className="p-8">
-      <ul className="flex gap-12">
+      <ul className="flex gap-12 justify-center">
         {links.map(link => {
           return (
-            <li key={link.path}>
-              <Link className="font-medium text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200" href={link.path}>
-                <span>{link.name}</span>
-              </Link>
-            </li>
+            <motion.li key={link.path}>
+              <MotionLink
+                className={cn(
+                  "font-medium rounded-lg text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200",
+                  pathname === link.path ? "bg-slate-300" : ""
+                )} href={link.path}
+              >
+                <motion.span>{link.name}</motion.span>
+              </MotionLink>
+            </motion.li>
           )
         })}
       </ul>
